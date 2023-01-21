acfab5c8f30427810d165c38026721c6b8da0837
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-22 00:31:30 +0900

    Elementライブラリーをプラグインで利用しよう

---
+++ pages/element.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <el-button @click="visible = true">Button</el-button>
+    <el-dialog :visible.sync="visible"><p>処理されました</p></el-dialog>
+  </div>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      visible: false,
+    };
+  },
+};
+</script>

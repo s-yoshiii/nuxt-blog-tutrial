22d9d6cac53c21aa5418f159546b4f26d48b792e
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-23 00:19:34 +0900

    axiosモジュール設定下準備

---
+++ pages/axios.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>{{ ip }}</p>
+    <button>IPアドレスを取得</button>
+  </div>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      ip: "0.0.0.0",
+    };
+  },
+};
+</script>
